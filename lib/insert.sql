7bf0a05a6b770c6b052ef0b95f9f66956c60a0cc
Emily <emilybribragg@gmail.com>
2022-06-20 08:37:52 -0400

    commit tree 7910ae15e71854521af60083386406210f04a320

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 5, "M", "brown", "calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 20, "F", "black", "moderate", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 2, "M", "white", "angry", 1);
+INSERT INTO bears VALUES (4,"Seargeant Brown", 15, "M", "brown", "angry", 1);
+INSERT INTO bears VALUES (5,"Melissa", 14, "F", "black", "calm", 1);
+INSERT INTO bears VALUES (6,"Grinch", 8, "M", "white", "moderate", 1);
+INSERT INTO bears VALUES (7,"Wendy", 22, "F", "brown", "calm", 1);
+INSERT INTO bears VALUES (8,NULL, 18, "F", "black", "calm", 1);
